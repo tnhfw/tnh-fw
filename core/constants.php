b383fe222c7e7b66fc165155c9ed561bf7365d03
Tony NGUEREZA <nguerezatony@gmail.com>
2017-05-15 10:01:25 +0100

    initial commit

--- core/constants.php
+++ core/constants.php
@@ -5,4 +5,4 @@ define('TNH_BUILD_DATE', '05/02/2017');
 define('TNH_AUTHOR', 'Tony NGUEREZA');
 define('TNH_AUTHOR_EMAIL', 'nguerezatony@gmail.com');
 define('TNH_REQUIRED_PHP_MIN_VERSION', '5.3');
-define('TNH_REQUIRED_PHP_MAX_VERSION', '7');
+define('TNH_REQUIRED_PHP_MAX_VERSION', '7.0.0');
