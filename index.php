5f8863213218cf19354cae20d459429d94abdc52
Tony NGUEREZA <nguerezatony@gmail.com>
2017-10-27 09:25:16 +0100

    adding comment to the front controller

--- index.php
+++ index.php
@@ -24,67 +24,160 @@
 */
 
 
-//the directory separator
+/* 
+the directory separator, under windows it is \ and unix, linux /
+*/
 define('DS', DIRECTORY_SEPARATOR);
 
-//the root path
+/*
+The root directory of the application.
+you can place this directory outside of your web directory, for example "/home/your_app", etc.
+*/
 define('ROOT_PATH', dirname(realpath(__FILE__)).DS);
 
-//the core path
+/* 
+The core directory
+It is recommended to put this folder out of the web directory of your server and 
+you should not change its content because in case of update you could lose the modified files.
+*/
 define('CORE_PATH', ROOT_PATH.'core'.DS);
 
-//the core libraries path
+/*
+The path to the system library directory 
+which contains the libraries most often used in your web application, as for the 
+core directory it is advisable to put it out of the root directory of your application.
+*/
 define('CORE_LIBRARY_PATH', CORE_PATH.'libraries'.DS);
 
-//the core views path
+/*
+The path to the system view directory 
+that contains the views used for the system, such as error messages, and so on.
+*/
 define('CORE_VIEWS_PATH', CORE_PATH.'views'.DS);
 
-//the config path
+/*
+The path to the configuration directory 
+that contains most of the configuration files for your 
+application (database, class loading file, functions, etc.)
+*/
 define('CONFIG_PATH', ROOT_PATH.'config'.DS);
 
-//the assets path
+/*
+The path to the directory
+ that contains your static files (javascript, css, images, etc.)
+Note: the path must be relative to the file index.php (the front-end controller).
+*/
 define('ASSETS_PATH', 'assets/');
 
-//the libraries path
+/*
+The path to the directory 
+of your personal libraries, it contains your PHP classes, package, etc.
+Note: you should not put your personal libraries in the system library directory, 
+because it is recalled in case of updating the system you might have surprises.
+*/
 define('LIBRARY_PATH', ROOT_PATH.'libraries'.DS);
 
-//the functions path
-define('FUNCTIONS_PATH', ROOT_PATH.'functions'.DS);
 
-//the core functions path
+/*
+The path to the directory 
+of functions or helper systems, it contains PHP functions that perform a particular task: 
+character string processing, URL, etc.
+*/
 define('CORE_FUNCTIONS_PATH', CORE_PATH.'functions'.DS);
 
 
-//the vendor path
+/*
+The path to the directory 
+of your PHP personal functions or helper, it contains your PHP functions 
+that perform a particular task: utilities, etc.
+Note: Do not put your personal functions or helpers in the system functions directory, 
+because if you update the system you may lose them.
+*/
+define('FUNCTIONS_PATH', ROOT_PATH.'functions'.DS);
+
+
+/*
+The path to the directory 
+of sources external to your application, if you have already used "composer" you know what that means.
+*/
 define('VENDOR_PATH', ROOT_PATH.'vendor'.DS);
 
-//the classes path
+/*
+The path to the directory 
+of your most often used files that is to say which contains your files of the application, 
+in MVC architecture (controllers, models, views).
+*/
 define('APPS_PATH', ROOT_PATH.'classes'.DS);
 
-//the controller path
+/*
+The path to the controller directory 
+of your application, if you already know the MVC architecture 
+you know what a controller means; it is he who makes the business logic of your application in general.
+*/
 define('APPS_CONTROLLER_PATH', APPS_PATH.'controllers'.DS);
 
-//the lang path
-define('LANG_PATH', CORE_PATH.'lang'.DS);
 
-//the model path
+/*
+The path to the directory 
+of your model classes of your application, 
+if you already know the MVC architecture you know what a model means; 
+it's the one who interacts with the database, in one word persistent data from your application.
+*/
 define('APPS_MODEL_PATH', APPS_PATH.'models'.DS);
 
-//the views path
+/*
+The path to the directory of your views, 
+if you already know the MVC architecture you know what a view means, 
+a view is just a user interface (html page, form, etc.) that is to say 
+everything displayed in the browser interface, etc.
+*/
 define('APPS_VIEWS_PATH', APPS_PATH.'views'.DS);
 
-//the cache path
+
+/*
+The path to the directory of your languages, 
+this feature is not yet available. 
+You can help us do this if you are nice or wish to see the developed framework.
+*/
+define('LANG_PATH', CORE_PATH.'lang'.DS);
+
+/*
+The path to the directory of your cache files,
+this feature is not yet available.
+You can help us do it if you are nice or want to see the developed framework.
+*/
 define('CACHE_PATH', ROOT_PATH.'cache'.DS);
 
-//the logs path
+/*
+The path to the directory that contains the log files.
+Note: This directory must be available in writing and if possible must have as owner the user who launches your web server, 
+under unix or linux most often with the apache web server it is "www-data" or "httpd" even "nobody" for more
+ details see the documentation of your web server.
+Example for Unix or linux with apache web server:
+# chmod -R 700 /path/to/your/logs/directory/
+# chown -R www-data:www-data /path/to/your/logs/directory/
+*/
 define('LOGS_PATH', ROOT_PATH.'logs'.DS);
 
-//this file
+/*
+the front controller of your application 
+"index.php" it is through this file that all the requests come, there is a possibility to hidden it in the url of 
+your application by using the rewrite module URL of your web server .
+For example, under apache web server, there is a configuration example file that is located at the root 
+of your framework folder : "htaccess.txt" rename it to ".htaccess".
+*/
 define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
 
-//environment must be development, testing or production
-define('ENVIRONMENT', 'development');
 
+/*
+The environment of your application (production, test, development), 
+if your application is still in development you use the value "development" 
+so you will have the display of the error messages, etc. 
+Once you finish the development of your application that is to put it online 
+you change this value to "production" or "testing", in this case there will be deactivation of error messages, 
+the loading of the system, will be fast.
+*/
+define('ENVIRONMENT', 'development');
 
 
 switch (ENVIRONMENT){
@@ -108,5 +201,8 @@ switch (ENVIRONMENT){
 		echo 'The application environment is not set correctly.';
 		exit(1);
 }
-//let's go
+/*
+let's go
+All is correct we launch our application.
+*/
 require_once CORE_PATH.'bootstrap.php';
