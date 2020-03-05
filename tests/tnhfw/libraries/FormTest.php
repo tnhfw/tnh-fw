46666dfba1dcc30f2c9c5730d87684a0cc49234c
NGUEREZA Tony <nguerezatony@gmail.com>
2020-03-05 12:59:52 +0100

    Update README.MD and phpunit.xml.dist

--- tests/tnhfw/libraries/FormTest.php
+++ tests/tnhfw/libraries/FormTest.php
@@ -2,25 +2,6 @@
 		
 	class FormTest extends TnhTestCase
 	{
-		
-		public static function setUpBeforeClass()
-		{
-		}
-		
-		public static function tearDownAfterClass()
-		{
-			
-		}
-		
-		
-		protected function setUp()
-		{
-			
-		}
-
-		protected function tearDown()
-		{
-		}
 
 		public function testNotYet()
 		{
