30116c1b74cad3c7a727f7149666fbe4cc3ec3d0
nguereza tony <nguerezatony@gmail.com>
2020-03-15 16:14:12 +0100

    Update BaseClassTest.php

--- tests/tnhfw/classes/BaseClassTest.php
+++ tests/tnhfw/classes/BaseClassTest.php
@@ -7,32 +7,22 @@
      * @group core_classes
      * @group base
      */
-	class BaseClassTest extends TnhTestCase {	
+     class BaseClassTest extends TnhTestCase {	
 		
-		public function testSetLoggerSimple() {
+	public function testSetGetLogger() {
             $o = new BaseClass();
             $o->setLogger(null);
-			$this->assertNull($o->getLogger());
-		}
-        
-        public function testSetLoggerFromParamOrCreate() {
-            $o = new BaseClass();
-            $o = $this->runPrivateProtectedMethod($o, 'setLoggerFromParamOrCreate', array(null));
-            $this->assertInstanceOf('Log', $o->getLogger());
-		}
-        
-        
-        public function testSetDependenciesInstanceIsNull() {
-            $o = new BaseClass();
-            $o = $this->runPrivateProtectedMethod($o, 'setDependencyInstanceFromParamOrCreate', array('foo', null, 'Benchmark', 'libraries'));
-            $this->assertInstanceOf('Benchmark', $o->foo);
-		}
-        
-        public function testSetDependenciesInstanceIsNotNull() {
-            $o = new BaseClass();//here logger already set to Class::BaseClass
-            $o = $this->runPrivateProtectedMethod($o, 'setDependencyInstanceFromParamOrCreate', array('fooLogger', new Log()));
-            $this->assertInstanceOf('Log', $o->fooLogger);
-            $this->assertEquals('Class::BaseClass', $o->getLogger()->getLogger());
-		}
-
+	    $this->assertNull($o->getLogger());
+            $o->setLogger(new Log());
+            $this->assertInstanceOf('Log', $o->logger);
 	}
+        
+               
+        
+        public function testSetDependency() {
+            $o = new BaseClass();
+            $o = $this->runPrivateProtectedMethod($o, 'setDependency', array('foo', 'Benchmark', 'libraries'));
+            $this->assertInstanceOf('Benchmark', $o->foo);
+	}
+
+     }
